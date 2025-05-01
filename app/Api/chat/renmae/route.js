01b83be1e2f7b2bb6c077c3f5d4d343b9aeee7ac
reshinrs <reshinpaulrs@gmail.com>
2025-05-01 15:52:35 +0530

    update

--- app/Api/chat/renmae/route.js
+++ app/Api/chat/renmae/route.js
@@ -1,4 +1,4 @@
-import Chat from "@/models/Chat";
+import Chat from "../../../models/Chat";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import connectDB from "../../../config/db";
